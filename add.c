ddd0ab780136333b5ea4b78619acb846dcfc128f
AshwinSkariya <ashwin.skariya@s.amity.edu>
2025-05-23 13:02:42 +0530

    added b4

--- add.c
+++ add.c
@@ -1,25 +1,11 @@
 #include<stdio.h>
-int add(int x, int y) {
-    return x + y;
-}
-int main()
-{
-	int a, b;
-    printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
-    printf("Sum = %d\n", add(a, b));
-    return 0;
-
 int main () {
 	int a, b;
-    printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
-    printf("Difference = %d\n", a - b);
-int main() {
-	int a, b, c;
-    printf("Enter three numbers: ");
-    scanf("%d %d %d", &a, &b, &c);
-    printf("Sum = %d\n", a + b + c);
+    printf("Enter two integers: ");
+    if(scanf("%d %d", &a, &b) != 2) {
+        printf("Invalid input!\n");
+        return 1;
+    }
+    printf("Sum = %d\n", a + b);
     return 0;
 }
-
